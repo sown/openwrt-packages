3ba2d40e4b484457eec25306e5e6900d894ffd5b
David Young <david@thedavidyoung.co.uk>
2018-03-22 19:13:47 +0000

    Adds 10.5.0.0/16 to allowed RFC1918 subnets and changes 10.12.0.0/15 to 10.13.0.0/16

--- sown-core/files/etc/sown/firewall/chains/10_noprivate.sh
+++ sown-core/files/etc/sown/firewall/chains/10_noprivate.sh
@@ -8,7 +8,8 @@ echo "Forbidding private subnet access"
 /usr/sbin/iptables -A private_subnets -d 172.16.0.0/28 -j REJECT
 
 # Sown subnets are okay
-/usr/sbin/iptables -A private_subnets -d 10.12.0.0/15 -j ACCEPT
+/usr/sbin/iptables -A private_subnets -d 10.13.0.0/16 -j ACCEPT
+/usr/sbin/iptables -A private_subnets -d 10.5.0.0/16 -j ACCEPT
 /usr/sbin/iptables -A private_subnets -d 10.0.0.0/8 -j REJECT
 
 /usr/sbin/iptables -A FORWARD -j private_subnets
