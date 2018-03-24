75c46c23da03fa5747edb2c2085ccfd8a8fb9dfa
David Young <david@thedavidyoung.co.uk>
2018-03-24 01:07:03 +0000

    Fixes netmask on 172.16.0.0/12 network

--- sown-core/files/etc/sown/firewall/chains/10_noprivate.sh
+++ sown-core/files/etc/sown/firewall/chains/10_noprivate.sh
@@ -5,7 +5,7 @@ echo "Forbidding private subnet access"
 /usr/sbin/iptables -N private_subnets
 
 /usr/sbin/iptables -A private_subnets -d 192.168.0.0/16 -j REJECT
-/usr/sbin/iptables -A private_subnets -d 172.16.0.0/28 -j REJECT
+/usr/sbin/iptables -A private_subnets -d 172.16.0.0/12 -j REJECT
 
 # Sown subnets are okay
 /usr/sbin/iptables -A private_subnets -d 10.13.0.0/16 -j ACCEPT
